028abcf4e745957fb58a099aa2965a3b08e3029d
LucasKron <lucaskronbauer16@gmail.com>
2026-09-19 13:58:53 -0300

    primeiro commit

---
+++ src/pages/dashboard/visao-geral/visao-geral.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './visao-geral.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+
+  </StrictMode>,
+)
